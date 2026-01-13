90ca52f536cbb28eea5c5421d7fa659066383292
Tomas Olvecky <tomas.olvecky@gmail.com>
2026-01-13 22:51:53 +0100

    r: Add link to github

--- scripts/collect-for-obelisk-generate.sh
+++ scripts/collect-for-obelisk-generate.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 cd "$(dirname "$0")/.."
 
-find . -type f -name "obelisk-oci.toml" | sort | while read -r file; do
-  echo "# see ${file}"
+find . -type f -name "obelisk-oci.toml" -printf '%P\n' | sort | while read -r file; do
+  echo "# see https://github.com/obeli-sk/components/tree/main/$(dirname ${file})"
   awk '
     /^\[\[/ { start=1 }
     start { print "# " $0 }
